724d0854fce8b6c35f98ee3644c2b6604d65a5d9
atruong18 <34152240+atruong18@users.noreply.github.com>
2018-03-18 19:22:17 -0500

    temp
    test

---
+++ resources/js/index.js
@@ -0,0 +1,334 @@
+new Vue({
+  el: '#app',
+  data: {
+  	items: 
+
+
+
+
+[
+	{
+		"itemName": "Proof of Hero",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/a/a7/Heros_proof.png",
+		"dropLoc": [
+			{
+				"area": "Okeanos",
+				"quest": "Pirate Ship",
+				"ap": 12,
+				"apPerDrop": 20.2,
+				"dropRate": 0.596
+			},
+			{
+				"area": "Orleans",
+				"quest": "Marseille",
+				"ap": 7,
+				"apPerDrop": 22.8,
+				"dropRate": 0.306
+			},
+			{
+				"area": "America",
+				"quest": "Dallas",
+				"ap": 17,
+				"apPerDrop": 25.0,
+				"dropRate": 0.679
+			}
+		]
+	},
+	{
+		"itemName": "Unlucky Bone",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/c/ca/Unlucky_bone.png",
+		"dropLoc": [
+			{
+				"area": "Fuyuki",
+				"quest": "X-C",
+				"ap": 4,
+				"apPerDrop": 21.7,
+				"dropRate": 0.184
+			},
+			{
+				"area": "Fuyuki",
+				"quest": "X-G",
+				"ap": 15,
+				"apPerDrop": 23.4,
+				"dropRate": 0.641
+			}
+		]
+	},
+	{
+		"itemName": "Dragon Fang",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/6/6f/Dragon_fang.png",
+		"dropLoc": [
+			{
+				"area": "America",
+				"quest": "Deming",
+				"ap": 17,
+				"apPerDrop": 27.1,
+				"dropRate": 0.627
+			},
+			{
+				"area": "Okeanos",
+				"quest": "Island of Wyverns",
+				"ap": 14,
+				"apPerDrop": 27.8,
+				"dropRate": 0.503
+			}
+		]
+	},
+	{
+		"itemName": "Void Dust",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/4/43/Voids_refuse.png",
+		"dropLoc": [
+			{
+				"area": "America",
+				"quest": "Charlotte",
+				"ap": 20,
+				"apPerDrop": 31.4,
+				"dropRate": 0.637
+			}
+		]
+	},
+// Silver
+	{
+		"itemName": "Seed of Yggdrasil",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/4/41/Yggdrasil_seed.png",
+		"dropLoc": [
+			{
+				"area": "Okeanos",
+				"quest": "Bountiful Sea",
+				"ap": 18,
+				"apPerDrop": 58.4,
+				"dropRate": 0.308
+			}
+		]
+	},
+	{
+		"itemName": "Ghost Lantern",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/b/b1/Ghost_lantern.png",
+		"dropLoc": [
+			{
+				"area": "Okeanos",
+				"quest": "Stormy Seas",
+				"ap": 15,
+				"apPerDrop": 67.3,
+				"dropRate": 0.223
+			}
+		]
+	},
+	{
+		"itemName": "Octuplet Twin Crystals",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/8/84/Octuplet_twin_crystals.png",
+		"dropLoc": [
+			{
+				"area": "Okeanos",
+				"quest": "Two-Current Sea",
+				"ap": 14,
+				"apPerDrop": 126.4,
+				"dropRate": 0.111
+			}
+		]
+	},
+	{
+		"itemName": "Serpent Jewel",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/2/28/Snake_jewel.png",
+		"dropLoc": [
+			{
+				"area": "Okeanos",
+				"quest": "Sunken Rock Seas",
+				"ap": 17,
+				"apPerDrop": 106.9,
+				"dropRate": 0.159
+			}
+		]
+	},
+	{
+		"itemName": "Phoenix Feather",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/c/c4/Phoenix_plume.png",
+		"dropLoc": [
+			{
+				"area": "Chaldea (Sun)",
+				"quest": "Saber 30AP",
+				"ap": 30,
+				"apPerDrop": 172.9,
+				"dropRate": 0.174
+			},
+			{
+				"area": "Okeanos",
+				"quest": "Bountiful Sea",
+				"ap": 18,
+				"apPerDrop": 192.8,
+				"dropRate": 0.093
+			}
+		]
+	},
+	{
+		"itemName": "Eternal Gear",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/d/d5/Infinity_gear.png",
+		"dropLoc": [
+			{
+				"area": "America",
+				"quest": "Chicago",
+				"ap": 21,
+				"apPerDrop": 51.4,
+				"dropRate": 0.409
+			},
+			{
+				"area": "London",
+				"quest": "Clerkenwell",
+				"ap": 18,
+				"apPerDrop": 68.1,
+				"dropRate": 0.264
+			}
+		]
+	},
+	{
+		"itemName": "Forbidden Page",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/e/e6/Forbidden_page.png",
+		"dropLoc": [
+			{
+				"area": "London",
+				"quest": "Hyde Park",
+				"ap": 20,
+				"apPerDrop": 72.8,
+				"dropRate": 0.275
+			}
+		]
+	},
+	{
+		"itemName": "Homunculus Baby",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/3/3c/Homunculus_baby.png",
+		"dropLoc": [
+			{
+				"area": "London",
+				"quest": "Southwark",
+				"ap": 19,
+				"apPerDrop": 61.4,
+				"dropRate": 0.310
+			}
+		]
+	},
+	{
+		"itemName": "Meteor Horseshoe",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/3/34/Meteoric_horseshoe.png",
+		"dropLoc": [
+			{
+				"area": "Okeanos",
+				"quest": "Caldera Island",
+				"ap": 17,
+				"apPerDrop": 131.5,
+				"dropRate": 0.129
+			}
+		]
+	},
+// Gold
+	{
+		"itemName": "Claw of Chaos",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/6/67/Talon_of_chaos.png",
+		"dropLoc": [
+			{
+				"area": "America",
+				"quest": "Des Moines",
+				"ap": 18,
+				"apPerDrop": 87.7,
+				"dropRate": 0.205
+			}
+		]
+	},
+	{
+		"itemName": "Heart of the Foreign God",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/e/ec/Heart_of_a_foreign_god.png",
+		"dropLoc": [
+			{
+				"area": "Chaldea (Fri)",
+				"quest": "Caster 30AP",
+				"ap": 30,
+				"apPerDrop": 500.0,
+				"dropRate": 0.060
+			},
+			{
+				"area": "Chaldea (Fri)",
+				"quest": "Caster 40AP",
+				"ap": 40,
+				"apPerDrop": 517.7,
+				"dropRate": 0.077
+			}
+		]
+	},
+	{
+		"itemName": "Dragon's Reverse Scale",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/c/c8/Dragons_reverse_scale.png",
+		"dropLoc": [
+			{
+				"area": "Chaldea (Thu)",
+				"quest": "Rider 40AP",
+				"ap": 40,
+				"apPerDrop": 476.1,
+				"dropRate": 0.084
+			},
+			{
+				"area": "Chaldea (Thu)",
+				"quest": "Rider 30AP",
+				"ap": 30,
+				"apPerDrop": 888.0,
+				"dropRate": 0.034
+			}
+		]
+	},
+	{
+		"itemName": "Spirit Root",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/7/7b/Spiritroot.png",
+		"dropLoc": [
+			{
+				"area": "America",
+				"quest": "Washington",
+				"ap": 20,
+				"apPerDrop": 281.7,
+				"dropRate": 0.071
+			}
+		]
+	},
+	{
+		"itemName": "War Horse's Small Horn",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/b/b9/Horsehorn.png",
+		"dropLoc": [
+			{
+				"area": "America",
+				"quest": "Learney",
+				"ap": 18,
+				"apPerDrop": 141.9,
+				"dropRate": 0.127
+			}
+		]
+	},
+	{
+		"itemName": "Tear Stone of Blood",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/f/ff/Bloodtear.png",
+		"dropLoc": [
+			{
+				"area": "America",
+				"quest": "Alexandria",
+				"ap": 18,
+				"apPerDrop": 176.0,
+				"dropRate": 0.102
+			}
+		]
+	},
+	{
+		"itemName": "Black Tallow",
+		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/4/49/Blacktallow.png",
+		"dropLoc": [
+			{
+				"area": "America",
+				"quest": "Lubbock",
+				"ap": 18,
+				"apPerDrop": 146.7,
+				"dropRate": 0.123
+			}
+		]
+	}
+]
+
+
+
+
+  }
+})
---
+++ temp/drop.js
@@ -0,0 +1,27 @@
+var data = {
+  items: [
+    {image: 'lnk', name: 'Hero\'s Proof', area: 'Okeanos', quest: 'Pirate Ship', ap: 12, rate: 0.598},
+    {image: 'lnk2', name: 'Unlucky Bone', area: 'Fuyuki', quest: 'X-C', ap: 4, rate: 18.2},
+    {image: 'lnk2', name: 'Dragon Tooth', area: 'Okeanos', quest: 'Wyvern Island', ap: 4, rate: 18.2},
+    {image: 'lnk2', name: 'Void Dust', area: 'Septem', quest: 'Massilia', ap: 4, rate: 18.2},
+
+    {image: 'lnk2', name: 'Yggdrasil Seed', area: 'Fuyuki', quest: 'XC', ap: 4, rate: 18.2},
+    {image: 'lnk2', name: 'Okeanos', area: 'Fuyuki', quest: 'XC', ap: 4, rate: 18.2},
+    {image: 'lnk2', name: 'UnluckyBone', area: 'Fuyuki', quest: 'XC', ap: 4, rate: 18.2},
+    {image: 'lnk2', name: 'UnluckyBone', area: 'Fuyuki', quest: 'XC', ap: 4, rate: 18.2},
+    {image: 'lnk2', name: 'UnluckyBone', area: 'Fuyuki', quest: 'XC', ap: 4, rate: 18.2},
+    {image: 'lnk2', name: 'UnluckyBone', area: 'Fuyuki', quest: 'XC', ap: 4, rate: 18.2},
+    {image: 'lnk2', name: 'UnluckyBone', area: 'Fuyuki', quest: 'XC', ap: 4, rate: 18.2},
+    {image: 'lnk2', name: 'UnluckyBone', area: 'Fuyuki', quest: 'XC', ap: 4, rate: 18.2},
+    {image: 'lnk2', name: 'UnluckyBone', area: 'Fuyuki', quest: 'XC', ap: 4, rate: 18.2},
+
+    {image: 'lnk2', name: 'UnluckyBone', area: 'Fuyuki', quest: 'XC', ap: 4, rate: 18.2},
+    {image: 'lnk2', name: 'UnluckyBone', area: 'Fuyuki', quest: 'XC', ap: 4, rate: 18.2},
+    {image: 'lnk2', name: 'UnluckyBone', area: 'Fuyuki', quest: 'XC', ap: 4, rate: 18.2}
+  ]
+}
+
+var vm = new Vue({
+  el: '#drop-table',
+  data: data
+})
