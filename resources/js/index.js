3bbb9c6f72a8a267fddcadeb5d27a0213a8f09f6
atruong18 <34152240+atruong18@users.noreply.github.com>
2018-04-16 18:39:58 -0500

    Navbar Updates
    New navbar stuff

--- resources/js/index.js
+++ resources/js/index.js
@@ -10,80 +10,84 @@ new Vue({
 	{
 		"itemName": "Proof of Hero",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/a/a7/Heros_proof.png",
+		"servants": "resources/img/servants-hero-proof.PNG",
 		"dropLoc": [
 			{
 				"area": "Okeanos",
 				"quest": "Pirate Ship",
 				"ap": 12,
 				"apPerDrop": 20.2,
-				"dropRate": 0.596
+				"dropRate": "59.6"
 			},
 			{
 				"area": "Orleans",
 				"quest": "Marseille",
 				"ap": 7,
 				"apPerDrop": 22.8,
-				"dropRate": 0.306
+				"dropRate": "30.6"
 			},
 			{
 				"area": "America",
 				"quest": "Dallas",
 				"ap": 17,
 				"apPerDrop": 25.0,
-				"dropRate": 0.679
+				"dropRate": "67.9"
 			}
 		]
 	},
 	{
 		"itemName": "Unlucky Bone",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/c/ca/Unlucky_bone.png",
+		"servants": "resources/img/servants-unlucky-bone.PNG",
 		"dropLoc": [
 			{
 				"area": "Fuyuki",
 				"quest": "X-C",
 				"ap": 4,
 				"apPerDrop": 21.7,
-				"dropRate": 0.184
+				"dropRate": "18.4"
 			},
 			{
 				"area": "Fuyuki",
 				"quest": "X-G",
 				"ap": 15,
 				"apPerDrop": 23.4,
-				"dropRate": 0.641
+				"dropRate": "64.1"
 			}
 		]
 	},
 	{
 		"itemName": "Dragon Fang",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/6/6f/Dragon_fang.png",
+		"servants": "resources/img/servants-dragon-fang.PNG",
 		"dropLoc": [
 			{
 				"area": "America",
 				"quest": "Deming",
 				"ap": 17,
 				"apPerDrop": 27.1,
-				"dropRate": 0.627
+				"dropRate": "62.7"
 			},
 			{
 				"area": "Okeanos",
 				"quest": "Island of Wyverns",
 				"ap": 14,
 				"apPerDrop": 27.8,
-				"dropRate": 0.503
+				"dropRate": "50.3"
 			}
 		]
 	},
 	{
 		"itemName": "Void Dust",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/4/43/Voids_refuse.png",
+		"servants": "resources/img/servants-void-dust.PNG",
 		"dropLoc": [
 			{
 				"area": "America",
 				"quest": "Charlotte",
 				"ap": 20,
 				"apPerDrop": 31.4,
-				"dropRate": 0.637
+				"dropRate": "63.7"
 			}
 		]
 	},
@@ -91,131 +95,140 @@ new Vue({
 	{
 		"itemName": "Seed of Yggdrasil",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/4/41/Yggdrasil_seed.png",
+		"servants": "resources/img/servants-seed.PNG",
 		"dropLoc": [
 			{
 				"area": "Okeanos",
 				"quest": "Bountiful Sea",
 				"ap": 18,
 				"apPerDrop": 58.4,
-				"dropRate": 0.308
+				"dropRate": "30.8"
 			}
 		]
 	},
 	{
 		"itemName": "Ghost Lantern",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/b/b1/Ghost_lantern.png",
+		"servants": "resources/img/servants-lantern.PNG",
 		"dropLoc": [
 			{
 				"area": "Okeanos",
 				"quest": "Stormy Seas",
 				"ap": 15,
 				"apPerDrop": 67.3,
-				"dropRate": 0.223
+				"dropRate": "22.3"
 			}
 		]
 	},
 	{
 		"itemName": "Octuplet Twin Crystals",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/8/84/Octuplet_twin_crystals.png",
+		"servants": "resources/img/servants-crystal.PNG",
 		"dropLoc": [
 			{
 				"area": "Okeanos",
 				"quest": "Two-Current Sea",
 				"ap": 14,
 				"apPerDrop": 126.4,
-				"dropRate": 0.111
+				"dropRate": "11.1"
 			}
 		]
 	},
 	{
 		"itemName": "Serpent Jewel",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/2/28/Snake_jewel.png",
+		"servants": "resources/img/servants-snake-jewel.PNG",
 		"dropLoc": [
 			{
 				"area": "Okeanos",
 				"quest": "Sunken Rock Seas",
 				"ap": 17,
 				"apPerDrop": 106.9,
-				"dropRate": 0.159
+				"dropRate": "15.9"
 			}
 		]
 	},
 	{
 		"itemName": "Phoenix Feather",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/c/c4/Phoenix_plume.png",
+		"servants": "resources/img/servants-plumes.PNG",
 		"dropLoc": [
 			{
 				"area": "Chaldea (Sun)",
 				"quest": "Saber 30AP",
 				"ap": 30,
 				"apPerDrop": 172.9,
-				"dropRate": 0.174
+				"dropRate": "17.4"
 			},
 			{
 				"area": "Okeanos",
 				"quest": "Bountiful Sea",
 				"ap": 18,
 				"apPerDrop": 192.8,
-				"dropRate": 0.093
+				"dropRate": "9.3"
 			}
 		]
 	},
 	{
 		"itemName": "Eternal Gear",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/d/d5/Infinity_gear.png",
+		"servants": "resources/img/servants-gear.PNG",
 		"dropLoc": [
 			{
 				"area": "America",
 				"quest": "Chicago",
 				"ap": 21,
 				"apPerDrop": 51.4,
-				"dropRate": 0.409
+				"dropRate": "40.9"
 			},
 			{
 				"area": "London",
 				"quest": "Clerkenwell",
 				"ap": 18,
 				"apPerDrop": 68.1,
-				"dropRate": 0.264
+				"dropRate": "26.4"
 			}
 		]
 	},
 	{
 		"itemName": "Forbidden Page",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/e/e6/Forbidden_page.png",
+		"servants": "resources/img/servants-pages.PNG",
 		"dropLoc": [
 			{
 				"area": "London",
 				"quest": "Hyde Park",
 				"ap": 20,
 				"apPerDrop": 72.8,
-				"dropRate": 0.275
+				"dropRate": "27.5"
 			}
 		]
 	},
 	{
 		"itemName": "Homunculus Baby",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/3/3c/Homunculus_baby.png",
+		"servants": "resources/img/servants-homunculus.PNG",
 		"dropLoc": [
 			{
 				"area": "London",
 				"quest": "Southwark",
 				"ap": 19,
 				"apPerDrop": 61.4,
-				"dropRate": 0.310
+				"dropRate": "31.0"
 			}
 		]
 	},
 	{
 		"itemName": "Meteor Horseshoe",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/3/34/Meteoric_horseshoe.png",
+		"servants": "resources/img/servants-horseshoe.PNG",
 		"dropLoc": [
 			{
 				"area": "Okeanos",
 				"quest": "Caldera Island",
 				"ap": 17,
 				"apPerDrop": 131.5,
-				"dropRate": 0.129
+				"dropRate": "12.9"
 			}
 		]
 	},
@@ -223,105 +236,112 @@ new Vue({
 	{
 		"itemName": "Claw of Chaos",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/6/67/Talon_of_chaos.png",
+		"servants": "resources/img/servants-claw.PNG",
 		"dropLoc": [
 			{
 				"area": "America",
 				"quest": "Des Moines",
 				"ap": 18,
 				"apPerDrop": 87.7,
-				"dropRate": 0.205
+				"dropRate": "20.5"
 			}
 		]
 	},
 	{
 		"itemName": "Heart of the Foreign God",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/e/ec/Heart_of_a_foreign_god.png",
+		"servants": "resources/img/servants-heart.PNG",
 		"dropLoc": [
 			{
 				"area": "Chaldea (Fri)",
 				"quest": "Caster 30AP",
 				"ap": 30,
 				"apPerDrop": 500.0,
-				"dropRate": 0.060
+				"dropRate": "6.0"
 			},
 			{
 				"area": "Chaldea (Fri)",
 				"quest": "Caster 40AP",
 				"ap": 40,
 				"apPerDrop": 517.7,
-				"dropRate": 0.077
+				"dropRate": "7.7"
 			}
 		]
 	},
 	{
 		"itemName": "Dragon's Reverse Scale",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/c/c8/Dragons_reverse_scale.png",
+		"servants": "resources/img/servants-scale.PNG",
 		"dropLoc": [
 			{
 				"area": "Chaldea (Thu)",
 				"quest": "Rider 40AP",
 				"ap": 40,
 				"apPerDrop": 476.1,
-				"dropRate": 0.084
+				"dropRate": "8.4"
 			},
 			{
 				"area": "Chaldea (Thu)",
 				"quest": "Rider 30AP",
 				"ap": 30,
 				"apPerDrop": 888.0,
-				"dropRate": 0.034
+				"dropRate": "3.4"
 			}
 		]
 	},
 	{
 		"itemName": "Spirit Root",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/7/7b/Spiritroot.png",
+		"servants": "resources/img/servants-root.PNG",
 		"dropLoc": [
 			{
 				"area": "America",
 				"quest": "Washington",
 				"ap": 20,
 				"apPerDrop": 281.7,
-				"dropRate": 0.071
+				"dropRate": "7.1"
 			}
 		]
 	},
 	{
 		"itemName": "War Horse's Small Horn",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/b/b9/Horsehorn.png",
+		"servants": "resources/img/servants-horn.PNG",
 		"dropLoc": [
 			{
 				"area": "America",
 				"quest": "Learney",
 				"ap": 18,
 				"apPerDrop": 141.9,
-				"dropRate": 0.127
+				"dropRate": "12.7"
 			}
 		]
 	},
 	{
 		"itemName": "Tear Stone of Blood",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/f/ff/Bloodtear.png",
+		"servants": "resources/img/servants-tears.PNG",
 		"dropLoc": [
 			{
 				"area": "America",
 				"quest": "Alexandria",
 				"ap": 18,
 				"apPerDrop": 176.0,
-				"dropRate": 0.102
+				"dropRate": "10.2"
 			}
 		]
 	},
 	{
 		"itemName": "Black Tallow",
 		"imageUrl": "https://vignette.wikia.nocookie.net/fategrandorder/images/4/49/Blacktallow.png",
+		"servants": "resources/img/servants-goop.PNG",
 		"dropLoc": [
 			{
 				"area": "America",
 				"quest": "Lubbock",
 				"ap": 18,
 				"apPerDrop": 146.7,
-				"dropRate": 0.123
+				"dropRate": "12.3"
 			}
 		]
 	}
